9d6780f7ef88da8e3eaa7022345458b45c3d160f
Shai Demri <shaidemri@gmail.com>
2020-08-11 14:46:07 +0300

    Updates

---
+++ precache-manifest.eadaf97955c6cfa205837e8c2da2ef5e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613d4cab5922785549875626a7cac603",
+    "url": "/Shai-Demri-05-08-2020/index.html"
+  },
+  {
+    "revision": "3a56abf1400535d65286",
+    "url": "/Shai-Demri-05-08-2020/static/js/0.23931e48.chunk.js"
+  },
+  {
+    "revision": "658a7e072f100aeb2a7e",
+    "url": "/Shai-Demri-05-08-2020/static/js/3.991c3455.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Shai-Demri-05-08-2020/static/js/3.991c3455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df0ee355f6fd8794039",
+    "url": "/Shai-Demri-05-08-2020/static/js/4.3ecf5d97.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/Shai-Demri-05-08-2020/static/js/4.3ecf5d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28cfb6561e4345685143",
+    "url": "/Shai-Demri-05-08-2020/static/js/5.afc5cb17.chunk.js"
+  },
+  {
+    "revision": "834e79ddaa3d54bd9de8",
+    "url": "/Shai-Demri-05-08-2020/static/js/6.d0fd0157.chunk.js"
+  },
+  {
+    "revision": "1f49be4f810345cb301c",
+    "url": "/Shai-Demri-05-08-2020/static/js/7.79e38cfe.chunk.js"
+  },
+  {
+    "revision": "198256bacd9bb7870a49",
+    "url": "/Shai-Demri-05-08-2020/static/js/8.8e0cd5f4.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/Shai-Demri-05-08-2020/static/js/8.8e0cd5f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920d87ce961007c779fb",
+    "url": "/Shai-Demri-05-08-2020/static/js/main.5350e185.chunk.js"
+  },
+  {
+    "revision": "97876c9cc2470fc6c474",
+    "url": "/Shai-Demri-05-08-2020/static/js/runtime-main.77277393.js"
+  },
+  {
+    "revision": "93430c7d951241623a2de4bd7ecc809f",
+    "url": "/Shai-Demri-05-08-2020/static/media/page-404.93430c7d.png"
+  }
+]);
